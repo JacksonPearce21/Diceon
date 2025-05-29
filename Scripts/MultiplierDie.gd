04caa6af9a62739da878e76daf96a01b9de5c0f2
JacksonPearce <21jpearce2@wakatipu.school.nz>
2025-05-29 14:48:31 +1200

    Fully sucesfull rolling D6
    Multiplier dice now displays the value on its face when it is landed on that value!!!! D10 is pain...

--- Scripts/MultiplierDie.gd
+++ Scripts/MultiplierDie.gd
@@ -1,11 +1,24 @@
 extends Node3D
 
-@export var sides = [1.0, 1.5, 2, 2.5, 3, 3.5]
-var current_face_value = 1.0
+@export var sides := {
+	1: {"rotation": Vector3(0, deg_to_rad(90), deg_to_rad(-90))},
+	2: {"rotation": Vector3(0, deg_to_rad(180), 0)},
+	3: {"rotation": Vector3(0, deg_to_rad(-90), 0)},
+	4: {"rotation": Vector3(0, deg_to_rad(90), 0)},
+	5: {"rotation": Vector3(0, 0, 0)},
+	6: {"rotation": Vector3(0, deg_to_rad(90), deg_to_rad(90))},
+}
+
+var current_face_value = 5
 
 func roll():
-	current_face_value = sides.pick_random()
+	current_face_value = sides.keys().pick_random()
 	tween_to_face(current_face_value)
 
-func tween_to_face(value):
-	print("Rolling MULT to face with value:", value)
+func tween_to_face(value: int):
+	var face_data = sides.get(value)
+	if face_data:
+		print("Rolling MULT to face:", value)
+		var rotation = face_data["rotation"]
+		var tween = create_tween()
+		tween.tween_property(self, "rotation", rotation, 0.8).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
