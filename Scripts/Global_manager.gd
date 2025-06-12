cfc317d758d025e2f177440b835588de82dff6f8
JacksonPearce <21jpearce2@wakatipu.school.nz>
2025-06-12 15:09:35 +1200

    better UI and global script

---
+++ Scripts/Global_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var round_num = 1
+var dice_rolls = 4
+var required_score = (50 * (round_num * 1.5)) + 25
--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -2,12 +2,21 @@ extends Node2D
 
 @onready var int_dice = $CanvasLayer/SubViewportContainer/IntViewport/interger_die
 @onready var mult_dice = $CanvasLayer/SubViewportContainer2/MultViewport/multiplier_die
-@onready var score_label = $Control/SideBar/ScoreLabel
+@onready var score_label = $CanvasLayer/Added_score
+@onready var score =$Control/SideBar/Score
+@onready var rolls =$Control/SideBar/Rolls
+@onready var required_score =$Control/SideBar/Required_score
+
+
+
 
 func _ready():
+	rolls.text = str(GlobalManager.dice_rolls)
 	pass
 
 func roll_all_dice():
+	GlobalManager.dice_rolls =- 1
+	rolls.text = str(GlobalManager.dice_rolls)
 	int_dice.roll()
 	mult_dice.roll()
 	await get_tree().create_timer(1.9).timeout
@@ -17,8 +26,12 @@ func calculate_score():
 	var base = int_dice.current_face_value
 	var mult = mult_dice.current_face_value
 	var total = base * mult
-	score_label.text = "Score: %.1f" % total
+	score_label.text = "+ %.0f" % total
 
 
 func _on_button_pressed():
+	GlobalManager.dice_rolls -= 1
+	rolls.text = str(GlobalManager.dice_rolls)
 	roll_all_dice()
+	
+	
