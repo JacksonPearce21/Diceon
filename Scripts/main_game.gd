e059081fab49979a540dfcc05741fab6a7fbc4c6
JacksonPearce <21jpearce2@wakatipu.school.nz>
2025-06-13 11:17:40 +1200

    Round score now updates

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -2,21 +2,23 @@ extends Node2D
 
 @onready var int_dice = $CanvasLayer/SubViewportContainer/IntViewport/interger_die
 @onready var mult_dice = $CanvasLayer/SubViewportContainer2/MultViewport/multiplier_die
-@onready var score_label = $CanvasLayer/Added_score
-@onready var score = $Control/SideBar/Score
-@onready var rolls = $Control/SideBar/Rolls
-@onready var required_score = $Control/SideBar/Required_score
-@onready var round_num = $Control/SideBar/Round_num
-
+@onready var added_score_label = $CanvasLayer/Added_score
+@onready var score_label = $Control/SideBar/Score
+@onready var rolls_label = $Control/SideBar/Rolls
+@onready var required_score_label = $Control/SideBar/Required_score
+@onready var round_num_label = $Control/SideBar/Round_num
+var round_score = 0
 
 func _ready():
-	round_num.text = str(GlobalManager.current_round)
-	rolls.text = str(GlobalManager.dice_rolls)
-	required_score = str(GlobalManager.score_needed)
+	round_num_label.text = str(GlobalManager.current_round)
+	rolls_label.text = str(GlobalManager.dice_rolls)
+	required_score_label = str(GlobalManager.score_needed)
+	score_label.text = str(round_score)
 	pass
 
 func roll_all_dice():
 	GlobalManager.decrease_roll()
+	rolls_label.text = str(GlobalManager.dice_rolls)
 	int_dice.roll()
 	mult_dice.roll()
 	await get_tree().create_timer(1.9).timeout
@@ -26,9 +28,10 @@ func calculate_score():
 	var base = int_dice.current_face_value
 	var mult = mult_dice.current_face_value
 	var total = base * mult
-	score_label.text = "+ %.0f" % total
+	added_score_label.text = "+ %.0f" % total
+	round_score += total
+	score_label.text = str(round_score)
 
 
 func _on_button_pressed():
-	rolls.text = str(GlobalManager.dice_rolls)
 	roll_all_dice()
