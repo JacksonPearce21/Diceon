417536858af39a724c6a6481d0a018b9f11fd082
JacksonPearce <21jpearce2@wakatipu.school.nz>
2025-07-22 13:22:45 +1200

    Some reason not working still

--- Scripts/high_roller_card.gd
+++ Scripts/high_roller_card.gd
@@ -1,12 +1,26 @@
-extends Node2D
+extends Panel
 
 @onready var description_box = $Discription_Box
-@onready var card = $AceOfSpades2
 
 func _ready():
-	description_box.visable = false
+	add_to_group("Card")
+	description_box.visible = false
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		if card.has_point(get_global_mouse_position()):
+		if get_rect().has_point(get_global_mouse_position()):
 			CardManager.select_card(self)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var hovered = get_viewport().gui_get_hovered_control()
+		if hovered == null or not hovered.is_in_group("Card"):
+			if CardManager.current_card:
+				CardManager.current_card.hide_description()
+				CardManager.current_card = null
+
+func show_description():
+	description_box.visible = true
+
+func hide_description():
+	description_box.visible = false
